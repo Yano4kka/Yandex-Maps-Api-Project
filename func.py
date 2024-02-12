9eb15b879f424b8525f482b615d5dd684fc19f4c
Yana <yanakalsina11@gmail.com>
2024-02-12 23:22:13 +0300

    9 задача. Добавлена функция переключателя (вкл/выкл) почтового индекса к полному адресу объекта.

--- func.py
+++ func.py
@@ -1,6 +1,8 @@
 import pygame
 import os
 
+pygame.init()
+
 
 def print_on_screen(screen, text):
     font = pygame.font.Font(None, text[3])
@@ -11,20 +13,30 @@ def print_on_screen(screen, text):
     screen.blit(string_rendered, intro_rect)
 
 
-def load_image(name):  # функция для загрузки картинки (взята из учебника)
+def load_image(name, colorkey=None):  # функция для загрузки картинки (взята из учебника)
     fullname = os.path.join('data', name)
     if name == 'сброс.jpg':
         image = pygame.image.load(fullname)
         image = pygame.transform.scale(image, (30, 30))
+    elif name == 'mail_index_off.png' or name == 'mail_index_on.png':
+        image = pygame.image.load(fullname)
+        image = pygame.transform.scale(image, (30, 30))
     else:
         image = pygame.image.load(fullname)
+
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
     return image
 
 
 class Options(pygame.sprite.Sprite):
     def __init__(self, im, x, y, options_group):
         super().__init__(options_group)
-        self.image = load_image(im)
+        self.image = load_image(im, 1)
         self.rect = self.image.get_rect().move(x, y)
         self.im = im
 
@@ -32,6 +44,6 @@ class Options(pygame.sprite.Sprite):
 class Options2(pygame.sprite.Sprite):
     def __init__(self, im, x, y, options_group):
         super().__init__(options_group)
-        self.image = load_image(im)
+        self.image = load_image(im, 1)
         self.rect = self.image.get_rect().move(x, y)
         self.im = im
